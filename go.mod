c1a17816c00c157670d1c767320a79a5ae8a23f4
Paolo Cuffiani <paolo.cuffiani@chialab.it>
2023-11-07 17:06:16 +0100

    bump Go version to 1.21, test build succeeds, submit Go dependencies

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/chialab/aws-ecr-get-login-password
 
-go 1.20
+go 1.21
 
 require (
 	github.com/aws/aws-sdk-go-v2/config v1.22.1
--- main.go
+++ main.go
@@ -13,24 +13,34 @@ import (
 )
 
 func main() {
+	if token, err := getToken(); err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println(token)
+	}
+}
+
+// Retrieve token for authentication against ECR registries.
+func getToken() (string, error) {
 	os.Setenv("AWS_SDK_LOAD_CONFIG", "1")
 	cfg, err := config.LoadDefaultConfig(context.TODO())
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	svc := ecr.NewFromConfig(cfg)
 	token, err := svc.GetAuthorizationToken(context.TODO(), &ecr.GetAuthorizationTokenInput{})
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	authData := token.AuthorizationData[0].AuthorizationToken
 	data, err := base64.StdEncoding.DecodeString(*authData)
 	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
 	parts := strings.SplitN(string(data), ":", 2)
-	fmt.Println(parts[1])
+
+	return parts[1], nil
 }
